c7a742cf800316434c2d7a1f16df2c6925f2bfd4
Dragonek20 <dragonr@wp.pl>
2018-07-20 09:43:35 +0200

    ok

--- removeFile.php
+++
@@ -1,8 +0,0 @@
-<?php
-/***********************************************************************
-  Copyright (C) Keos Media
-************************************************************************/
-require_once('init.php'); 
-require_once('lib'. DS .'functions.php');
-
-new RemoveFile($_REQUEST['path']);
